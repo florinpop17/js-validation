e9223098f5d9de11faf48bee1a900e72fc26c8ad
Florin Pop <popflorin1705@yahoo.com>
2017-02-23 16:51:50 +0200

    form validation js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 var form = document.getElementById('add-form');
+var inputs = document.querySelectorAll('input[type="text"]');
+var selects = document.querySelectorAll('select');
+var damagedCheckbox = document.getElementById('damaged');
 
 // First we reset the form to default
 form.reset();
@@ -6,4 +9,25 @@ form.reset();
 // Form validation on submit
 form.onsubmit = function(e){
     e.preventDefault();
+
+    if(formIsValid()){
+        alert('All good!');
+    } else {
+        alert('something is wrong');
+    }
+}
+
+function formIsValid() {
+    var allValid = true;
+    inputs.forEach(input => {
+        if(input.value === ''){
+            allValid = false;
+        }
+    });
+    selects.forEach(select => {
+        if(select.value === 'no'){
+            allValid = false;
+        }
+    });
+    return allValid;
 }
